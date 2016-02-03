447328d5b44ba2ce32bed97a68cc73e1c6df989f
TERAMATE KARNDUMRI <teramate.karn@bulive.net>
2016-02-03 12:17:39 +0700

    CREATING HAZARDS

---
+++ test/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+	
+	void OnTriggerEnter(Collider other){
+		if (other.tag == "Boundary") 
+		{
+			return;
+		}
+		Destroy (other.gameObject);
+		Destroy (gameObject);
+	}
+	
+}﻿
