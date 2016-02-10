7195dc8b53f89224ef43f5629cf34ea0bfbd997e
TERAMATE KARNDUMRI <teramate.karn@bulive.net>
2016-02-10 11:23:12 +0700

    EXPLOSIONS

--- test/Assets/Scripts/DestroyByContact.cs
+++ test/Assets/Scripts/DestroyByContact.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DestroyByContact : MonoBehaviour {
+public class DestroyByContact : MonoBehaviour
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
 	
-	void OnTriggerEnter(Collider other){
-		if (other.tag == "Boundary") 
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.tag == "Boundary")
 		{
 			return;
 		}
-		Destroy (other.gameObject);
-		Destroy (gameObject);
+		Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player")
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
+		Destroy(other.gameObject);
+		Destroy(gameObject);
 	}
-	
-}﻿
+}
--- test/Assets/Scripts/RandomRotator.cs
+++ test/Assets/Scripts/RandomRotator.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class RandomRotator : MonoBehaviour
 {
 	public float tumble;
-	public Rigidbody rb;
-	
+	private Rigidbody rb;
+
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
