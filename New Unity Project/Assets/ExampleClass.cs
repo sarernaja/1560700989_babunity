61b3be884981001f8e66b9a2286b67cbe874c6df
TERAMATE KARNDUMRI <teramate.karn@bulive.net>
2016-02-17 10:30:25 +0700

    no message

---
+++ New Unity Project/Assets/ExampleClass.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour {
+	public Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		if (Input.GetButtonDown("Jump"))
+			rb.velocity = new Vector3(0, 4, 0);
+		
+	}
+}
