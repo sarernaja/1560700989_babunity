b32c2431d3acc218f730d11102133e5d9db2b8f5
TERAMATE KARNDUMRI <teramate.karn@bulive.net>
2016-02-24 12:49:46 +0700

    Game jam

---
+++ Gamejam/Assets/Kadod.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kadod : MonoBehaviour {
+	public Rigidbody rb;
+	public GameObject Button;
+	public GameObject PlayerDeadPrefab;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+	}
+	void FixedUpdate() {
+		if (Input.GetButtonDown("Jump"))
+			rb.velocity = new Vector3(0, 5, 0);
+		
+	}
+	void Update(){
+		if (rb.velocity.y < 0) {
+			lastVelocity = rb.velocity.y;
+		}
+	}
+	float lastVelocity;
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "floor") {
+//			float currentvelocity = rb.velocity.y;
+			Debug.LogFormat ("currentvelocity = {0}", lastVelocity);
+			rb.velocity = new Vector3 (0, -lastVelocity, 0);
+		} else if (collision.gameObject.tag == "ceil") {
+			Instantiate(PlayerDeadPrefab,this.gameObject.transform.position,Quaternion.identity);
+			Destroy (this.gameObject);
+			Button.SetActive(true);
+		}
+	}
+
+
+
+}
---
+++ Gamejam/Assets/Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	public float speed;
+	public Rigidbody rb;
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.tag == "cly") {
+			rb = GetComponent<Rigidbody> ();
+			rb.velocity = transform.forward * speed;
+		}
+		
+	}
+	
+	
+}﻿
