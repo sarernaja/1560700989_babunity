a176e111fbbdf8ca54aa207e5eb602a93ddeee60
sarernaja <teramate.karn@bumail.net>
2016-04-27 04:31:38 +0700

    pre

---
+++ Prefinal/Assets/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	IEnumerator OnMouse()
+	{
+		yield return new WaitForSeconds (1f);
+		Debug.Log ("isClick");
+		Application.LoadLevel(0);
+	}
+	// Use this for initialization
+	void Start () {
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
--- Prefinal/Assets/menu.cs
+++ Prefinal/Assets/menu.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class menu : MonoBehaviour {
 
+	IEnumerator OnMouseDown()
+	{
+		yield return new WaitForSeconds (1f);
+		Debug.Log ("isClick");
+		Application.LoadLevel(1);
+	}
 	// Use this for initialization
 	void Start () {
-		Application.LoadLevel("prefinal1");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Application.Quit();
+		
 	}
 }
